41ac0e06b4817437cd55e1208105f4e055bbde74
Ever Orlando Reyes Ruiz <eorlando.reyes@icloud.com>
2022-04-15 16:36:00 -0500

    Java Encapsulation

--- Java/Main.java
+++ Java/Main.java
@@ -1,8 +1,8 @@
 class Main {
     public static void main(String[] args) {
-        Car car = new Car("WNQ875", new Account("Fredy Gomez", "FG123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("WNQ875", new Account("Fredy Gomez", "FG123"), "Mazda", "323");
+        uberX.setPassenger(2);
+        uberX.printDataCar();
 
     }
 }
